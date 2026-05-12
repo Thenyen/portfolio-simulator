96f8b8de05746445ed0849cd43c886fd76bbe89c
Thenyen <schwerdtfeger.erice@gmail.com>
2026-05-12 23:52:33 +0200

    feat: add first plotly chart for AAPL

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from data.fetch import fetch_data
+import plotly.express as px
 
 df = fetch_data("AAPL", "2020-01-01", "2024-01-01")
 
-# Buy-and-Hold Rendite
-start_price = df.iloc[0].item()
-end_price = df.iloc[-1].item()
-total_return = (end_price - start_price) / start_price * 100
-
-print(f"Startkurs:  {start_price:.2f} $")
-print(f"Endkurs:    {end_price:.2f} $")
-print(f"Rendite:    {total_return:.2f} %")
+fig = px.line(df, title="AAPL Kursentwicklung 2020–2024")
+fig.show()
